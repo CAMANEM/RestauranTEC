2843710caa5a34acd8117af237e3bb0cf4ad0e07
seba21541@gmail.com <seba21541@gmail.com>
2023-05-29 16:38:45 -0600

    DataBase | Updated format
    updated format for the database, added the food and drink categories to sort the restaurant

--- src/code/database.pl
+++ src/code/database.pl
@@ -1,36 +1,79 @@
 % La informacion individual del restaurante tiene el siguiente formato:
 % [([nombre del restaurante]), (tipo de comida), ([provincia, canton]),
-% (direccion), ([menu disponible])]
+% (direccion), ([menu disponible]),(capacidad),(disposiciones)]
+%
+%
+% restaurante([[],"",[],"",[],"",""]).
 
 % En cartago:
-restaurante([[mcDonalds,'McDonalds'], rapida, ['Cartago','Caballo Blanco'],'150 m sur del walmart de caballo blanco',['BigMac','Cuarto de libra','Pollo Bacon','Papas fritas']]).
-restaurante([[subway,'Subway'], rapida,['Cartago','Oriental'],'500m oeste de la entrada principal del ITCR',['Jamon de pavo','Pollo teriyaki','Italiano B.M.T.','Desayuno completo']]).
+restaurante([
+    [mcDonalds,'McDonalds'],
+    "rapida",
+    ['Cartago','Caballo Blanco'],
+    "150 m sur del walmart de caballo blanco",
+    ['BigMac','Cuarto de libra','Pollo Bacon','Papas fritas'],
+     "30",
+     "Este es un restaurante familiar por lo que se debe tener un comportamiento acorde"]).
+
+restaurante([
+    [subway,'Subway'],
+    "saludable",
+    ['Cartago','Cartago'],
+    '500m oeste de la entrada principal del ITCR',
+    ['Jamon de pavo','Pollo teriyaki','Italiano B.M.T.','Desayuno completo'],
+    "20",
+    "Pregunta por el sub del dia, un delicioso sandwich a buen precio"]).
+
+/*
 restaurante([[laTortilleria,'La Tortilleria'] , tipica ,['Cartago','Cartago'],'175m oeste de la Basilica de Los Angeles',['Pintortilla con cafe','Torticasado','Tortilla de Queso','Chorreadas']]).
-restaurante([[balconCriollo,'Balcon Criollo'] ,tipica ,['Cartago','Cartago'],'Costado este de la municipalidad enavenida central de Cartago',['Chifrijo','Casados','Olla de carne','Hamburguesas']]).
+restaurante([[balconCriollo,'Balcon Criollo'] ,tipica
+,['Cartago','Cartago'],'Costado este de la municipalidad enavenida
+central de Cartago',['Chifrijo','Casados','Olla de
+carne','Hamburguesas']]).*/
 
 
 
 
 % La informacion de que restaurantes tienen ciertas comidas tiene el
 % siguiente formato:
-% [(tipo de comida), [(nombre de los restaurantes disponibles)]]
+% [(categoria),[(comidas disponibles)], [(nombre de los restaurantes
+% que ofrecen)]]
+%
+% comida("",[],[]).
 
 
-menu(['hamburguesa',[mcDonalds, balconCriollo]]).
+
+comida("rapida",["hamburguesa","papas fritas","sandwich"],[mcDonalds,subway]).
+comida("tipica",["torilla de queso","olla de carne"],[balconCriollo,laTortilleria]).
+
+/*menu(['hamburguesa',[mcDonalds, balconCriollo]]).
 menu(['hamburguesas',[mcDonalds, balconCriollo]]).
 menu(['tortilla',[laTortilleria, balconCriollo]]).
 menu(['tortillas',[laTortilleria, balconCriollo]]).
-menu(['sandwich',[subway, balconCriollo]]).
+menu(['sandwich',[subway, balconCriollo]]).*/
 
 
 
 
-% La informacion de que restaurantes tienen ciertas bebidas tiene el
+% La informacion sobre restaurantes que tienen ciertas bebidas tiene el
 % siguiente formato:
-% [(bebida), [(nombre de los restaurantes disponibles)]]
+% [(categoria),[(bebidas disponibles)], [(nombre de los restaurantes
+% disponibles)]]
+%
+% bebida("",[],[]).
 
-bebida(['coca cola',[mcDonalds, subway]]).
+
+bebida("natural",["cas","fresa"],[balconCriollo,laTortilleria]).
+bebida("caliente",["café","chocolate","agua dulce"],[balconCriollo , laTortilleria]).
+bebida("gaseosa",["coca cola","sprite","fanta"],[mcDonalds,subway]).
+bebida("coctel",["piña colada","margarita"],[balconCriollo]).
+bebida("cerveza",["nacional","corona"],[balconCriollo]).
+
+
+
+
+/*bebida(['coca cola',[mcDonalds, subway]]).
 bebida(['natural',[laTortilleria, balconCriollo]]).
 bebida(['cafe',[laTortilleria, balconCriollo, subway, mcDonalds]]).
 bebida(['agua dulce',[laTortilleria, balconCriollo]]).
-bebida(['cerveza',[balconCriollo]]).
+bebida(['cerveza',[balconCriollo]]).*/
