cfdb71f6951029016ff1963b411bc31c1e9a746f
seba21541@gmail.com <seba21541@gmail.com>
2023-05-30 22:27:34 -0600

    ES/DB | Format changes and more questions for ES
    Added new functions to the ES and new questions, changed format for the DataBase

--- src/code/database.pl
+++ src/code/database.pl
@@ -3,33 +3,42 @@
 % (direccion), ([menu disponible]),(capacidad),(disposiciones)]
 %
 %
-% restaurante([[],"",[],"",[],"",""]).
+% restaurante([[],'','','',[],'','']).
 
 % En cartago:
 restaurante([
-    [mcDonalds,'McDonalds'],
-    "rapida",
-    ['Cartago','Caballo Blanco'],
-    "150 m sur del walmart de caballo blanco",
+    ['mcdonalds','McDonalds'],
+    'rapida',
+    'Cartago',
+    '150 m sur del walmart de caballo blanco',
     ['Hamburguesa','Desayuno','Postre','Papas fritas'],
-     "30",
-     "Este es un restaurante para toda la familia, area de juego para niños"]).
+     '30',
+     'Este es un restaurante para toda la familia, area de juego para niños']).
 
 restaurante([
-    [subway,'Subway'],
-    "saludable",
+    ['subway','Subway'],
+    'saludable',
     ['Cartago','Cartago'],
     '500m oeste de la entrada principal del ITCR',
     ['Sandwich','Ensalada','Burrito','Desayuno'],
-    "20",
-    "Pregunta por el sub del dia, un delicioso sandwich a buen precio"]).
+    '20',
+    'Pregunta por el sub del dia, un delicioso sandwich a buen precio']).
 
 /*
-restaurante([[laTortilleria,'La Tortilleria'] , tipica ,['Cartago','Cartago'],'175m oeste de la Basilica de Los Angeles',['Pintortilla con cafe','Torticasado','Tortilla de Queso','Chorreadas']]).
+
 restaurante([[balconCriollo,'Balcon Criollo'] ,tipica
 ,['Cartago','Cartago'],'Costado este de la municipalidad enavenida
 central de Cartago',['Chifrijo','Casados','Olla de
 carne','Hamburguesas']]).*/
+
+% En San Jose
+
+
+
+
+% En Heredia
+
+
 
 
 
@@ -39,15 +48,15 @@ carne','Hamburguesas']]).*/
 % [(categoria),[(comidas disponibles)], [(nombre de los restaurantes
 % que ofrecen)]]
 %
-% comida(["",[],[]]).
+% comida(['',[],[]]).
 
 
 
-comida(["rapida",["hamburguesa","papas fritas","sandwich"],[mcDonalds,subway]]).
-comida(["tipica",["torilla de queso","olla de carne"],[balconCriollo,laTortilleria]]).
-comida(["saludable",["",""],[]]).
-comida(["italiana",[""],[]]).
-comida(["china",[""],[]]).
+comida(['rapida',['hamburguesa','papas fritas','sandwich'],[mcDonalds]]).
+comida(['tipica',['torilla de queso','olla de carne'],[balconCriollo]]).
+comida(['saludable',['',''],[]]).
+comida(['italiana',[''],[]]).
+comida(['china',[''],[]]).
 
 
 
@@ -57,12 +66,27 @@ comida(["china",[""],[]]).
 % [(categoria),[(bebidas disponibles)], [(nombre de los restaurantes
 % disponibles)]]
 %
-% bebida(["",[],[]]).
+% bebida(['',[],[]]).
 
 
-bebida(["natural",["cas","fresa"],[balconCriollo,laTortilleria]]).
-bebida(["caliente",["café","chocolate","agua dulce"],[balconCriollo , laTortilleria]]).
-bebida(["gaseosa",["coca cola","sprite","fanta"],[mcDonalds,subway]]).
-bebida(["coctel",["piña colada","margarita"],[balconCriollo]]).
-bebida(["cerveza",["nacional","corona"],[balconCriollo]]).
+bebida(['natural',['cas','fresa'],[balconCriollo]]).
+bebida(['caliente',['café','chocolate','agua dulce'],[balconCriollo]]).
+bebida(['gaseosa',['coca cola','sprite','fanta'],[mcDonalds,subway]]).
+bebida(['coctel',['piña colada','margarita'],[balconCriollo]]).
+bebida(['cerveza',['nacional','corona'],[balconCriollo]]).
+
+
+
+/*
+ *
+ *
+ *
+ */
+
+
+
+lugar(['Cartago',['']]).
+lugar(['cartago',['']]).
+lugar(['San Jose',['']]).
+lugar(['san jose',['']]).
 
--- src/code/es.pl
+++ src/code/es.pl
@@ -8,12 +8,6 @@
 |		     funciones atómicas                        |
 |______________________________________________________________|
 */
-list_to_string(List, String):-
-	atomic_list_concat(List, ' ', String).
-
-/*input_to_list(L):-
-	atom_codes(A,Cs),
-	atomic_list_concat(L,' ',A).*/
 
 
 
@@ -21,7 +15,11 @@ getComidas(C):- findall(X,(comida([X,_,_])),C).
 
 getBebidas(B):- findall(X,(bebida([X,_,_])),B).
 
-printCategorie([L|N]):-  write("* Comida "), write(L),nl, printCategorie(N).
+getLugares(L):- findall(X,(lugar([X,_])),L).
+
+
+
+printCategorie([L|N]):-  write("* "), write(L),nl,longitud(N,Len),( Len >= 1 ->printCategorie(N);!).
 
 
 miembro(X,[X|_]).
@@ -32,12 +30,19 @@ longitud([],0).
 longitud([_|R],Len):- longitud(R,Len1), Len is Len1+1.
 
 
-validateComida([],X):- nl,writeln('La comida que quiere no está disponible'),nl,writeln('Vuelva a intentarlo'),nl.
-
+validateComida([],X):- nl,writeln('La comida que quiere no está disponible'),nl,writeln('Vuelva a intentarlo'),nl, readln(Input),validateComida(Input,X).
 validateComida([H|_],X):- getComidas(C),miembro(H,C),X = H,!.
-validateComida([H|T],X):- getComidas(C),\+miembro(H,C),validateComida(T,X) .
+validateComida([H|T],X):- getComidas(C),\+miembro(H,C),validateComida(T,X).
 
-%validateBebida:- .
+
+validateBebida([],X):- nl,writeln('La bebida que quiere no está disponible'),nl,writeln('Vuelva a intentarlo'),nl, readln(Input),validateBebida(Input,X).
+validateBebida([H|_],X):- getBebidas(C),miembro(H,C),X = H,!.
+validateBebida([H|T],X):- getBebidas(C),\+miembro(H,C),validateBebida(T,X).
+
+
+validateLugar([],X):- nl,writeln('El lugar que quiere no está disponible'),nl,writeln('Vuelva a intentarlo'),nl, readln(Input),validateLugar(Input,X).
+validateLugar([H|_],X):- getLugares(C),miembro(H,C),X = H,!.
+validateLugar([H|T],X):- getLugares(C),\+miembro(H,C),validateLugar(T,X).
 
 /*
  ___________________________________________________
@@ -47,6 +52,8 @@ validateComida([H|T],X):- getComidas(C),\+miembro(H,C),validateComida(T,X) .
 */
 
 startES:-
+    writeln('
+'),
     writeln('Hola, ¡Bienvenido a RestauranTEC!'),
     nl,
     writeln("¿que tipo de comida se te antoja?"),
@@ -54,9 +61,26 @@ startES:-
     writeln("Tenemos disponible: "),nl,
     getComidas(Comidas),
     printCategorie(Comidas),
-    sleep(0.5),
-    read(Oracion1),
-
+    readln(Oracion1),
     validateComida(Oracion1,TipoComida),
-    nl,write('comida ingresada: '),write(TipoComida).
+    nl,write('comida ingresada: '),write(TipoComida),nl,
+
+    writeln("¿que tipo de bebida desea tomar?"),
+    nl,
+    writeln("Tenemos disponible: "),nl,
+    getBebidas(Bebidas),
+    printCategorie(Bebidas),
+    readln(Oracion2),
+    validateBebida(Oracion2,TipoBebida),
+    nl,write('bebida ingresada: '),write(TipoBebida),
+
+
+    writeln("¿En que zona está buscando el restaurante?"),
+    nl,
+    writeln("Los restaurantes que tenemos cubren: "),nl,
+    getLugares(Lug),
+    printCategorie(Lug),
+    readln(Oracion3),
+    validateLugar(Oracion3,Lugar),
+    nl,write('lugar ingresado: '),write(Lugar).
 
