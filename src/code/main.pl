6a72eb7cf0bfec24e3d2bc6e546f33c38abdfecc
CAMANEM <camanem10@hotmail.com>
2023-06-04 22:00:03 -0600

    automatic intialization from main

--- src/code/main.pl
+++ src/code/main.pl
@@ -1,11 +1,6 @@
-:-consult('bnf').
+:-consult('es').
 
 :- initialization(restauranTEC).
 
 restauranTEC:-
-    writeln('**************************************************'),
-    writeln('***************    RestauranTEC    ***************'),
-    writeln('**************************************************'),
-    writeln("Welcome to RestauranTEC, we'll make sure to find the perfect place for you!"),
-    writeln("Now, why don't you tell me if there's anything you'd like to have in particular"),
-    readln(Ans), sentence(Ans,[]).
+    startES.
