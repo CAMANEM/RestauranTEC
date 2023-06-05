8898b5813316b2e439a7601ed63fb75b9d6be3dc
seba21541@gmail.com <seba21541@gmail.com>
2023-06-05 14:28:10 -0600

    Method comments

--- src/expertsystem.pl
+++ src/expertsystem.pl
@@ -10,6 +10,8 @@
 */
 
 
+
+
 %   Obtiene todos los valores en la posicion X de un hecho en la base de
 %   datos y lo devuelve como una lista.
 get_comidas(C):- findall(X,(comida([X,_,_])),C).
@@ -21,13 +23,18 @@ get_bebidas2(B):-findall(X,(bebida([_,X,_])),B).
 get_lugares(L):- findall(X,(lugar([X,_,_])),L).
 get_lugares2(L):- findall(X,(lugar([_,X,_])),L).
 
+
+
+
 %  Imprime en consola una lista de valores y una lista de subvalores si
 %  se encuentra disponible.
 
 
-print_categorie([L|N]):-  write("* "), write(L),nl,longitud(N,Len),( Len >= 1 ->print_categorie(N);!).
+print_categorie([L|N]):-  write('*'), write(L),nl,longitud(N,Len),( Len >= 1 ->print_categorie(N);!).
+print_subcategorie([L|N]):-  write(' + '), write(L),nl,longitud(N,Len),( Len >= 1 ->print_subcategorie(N);!).
 
-print_subcategorie([L|N]):-  write(" + "), write(L),nl,longitud(N,Len),( Len >= 1 ->print_subcategorie(N);!).
+
+
 
 
 %  revisa si un valor es miembro de una lista de listas, solo devuelve
@@ -56,9 +63,15 @@ member_val(X,[],Z,Val):- Z = 0.
 member_val(X,[Y|_],Z,Val):- (X == Y -> Z = 1,Val = Y,!).
 member_val(X,[_|R],Z,Val):- member_val(X,R,Z,Val).
 
+
+
+
 %devuelve la longitud de una lista.
 longitud([],0).
 longitud([_|R],Len):- longitud(R,Len1), Len is Len1+1.
+
+
+
 
 
 is_question([],Z):- Z= 0 .
@@ -101,45 +114,55 @@ is_ayuda([_|N],Z):- is_ayuda(N,Z).
 
 
 
-%
-%
-%
+
+
+/*
+    Receives a sentences and sends the value to the correct validation, it returns the validated value.
+    @param        Oracion:  sentence sent by the user as a list.
+    @param        X:  checked core value of the sentence.
+
+*/
 validate_input(Oracion,X):- is_end(Oracion,Val),(Val ==1 ->halt()).
-validate_input(Oracion,X):- is_ayuda(Oracion,Val),(Val ==1 -> printAyuda(),nl,readln(Input),validate_input(Input,X)).
-validate_input(Oracion,X):- is_question(Oracion,Val),(Val ==1 ->validate_question(Oracion),nl,readln(Input),validate_input(Input,X)).
+validate_input(Oracion,X):- is_ayuda(Oracion,Val),(Val ==1 -> printAyuda(),nl,readln(Input),valid_sentence(Input,Inputver),validate_input(Inputver,X)).
+validate_input(Oracion,X):- is_question(Oracion,Val),(Val ==1 ->validate_question(Oracion),nl,readln(Input),valid_sentence(Input,Inputver),validate_input(Inputver,X)).
 validate_input(Oracion,X):- is_comida(Oracion,Val),(Val ==1 -> validate_comida(Oracion,Comida),X=Comida).
 validate_input(Oracion,X):- is_bebida(Oracion,Val),(Val ==1 -> validate_bebida(Oracion,Bebida),X=Bebida).
 validate_input(Oracion,X):- is_place(Oracion,Val),(Val ==1 -> validate_lugar(Oracion,Lugar),X=Lugar).
 validate_input(Oracion,X):- is_amount(Oracion,Val),(Val ==1 -> validate_amount(Oracion,Lugar),X=Lugar).
 
 
-% valida si la entrada del usuario es una comida disponible en la base
-% de datos, devuelve su valor revisado
+
+
+
+/*
+    all the validate_X receives the input sentence and checks if the value is in the database.
+    @param        List:  input sentece sent by the user as a list.
+    @param        X:  checked value from the database.
+
+*/
 validate_comida([],X):- nl,writeln('La comida que quiere no esta disponible'),nl,writeln('Vuelva a intentarlo'),nl, readln(Input),validate_comida(Input,X).
 validate_comida([H|_],X):- is_comida(H,Val,Z),(Z==1 -> X=Val ,!).
 validate_comida([H|T],X):- validate_comida(T,X).
 
-
-% valida si la entrada del usuario es una bebida disponible en la base
-% de datos, devuelve su valor revisado
 validate_bebida([],X):- nl,writeln('La bebida que quiere no esta disponible'),nl,writeln('Vuelva a intentarlo'),nl, readln(Input),valid_sentence(Input,InputVal),validate_bebida(InputVal,X).
 validate_bebida([H|_],X):-  is_bebida(H,Val,Z),(Z==1 -> X=Val ,!).
 validate_bebida([H|T],X):- validate_bebida(T,X).
 
-
-% valida si la entrada del usuario es un lugar disponible en la base
-% de datos, devuelve su valor revisado
 validate_lugar([],X):- nl,writeln('El lugar que quiere no esta disponible'),nl,writeln('Vuelva a intentarlo'),nl, readln(Input),valid_sentence(Input,InputVal),validate_lugar(InputVal,X).
 validate_lugar([H|_],X):- is_place(H,Val,Z),(Z==1 -> X=Val,!).
 validate_lugar([_|T],X):- validate_lugar(T,X).
 
-% Revisa si la entrada del usuario contiene un numero.
 validate_amount([],X):- nl,writeln('Ingrese un numero valido'),nl,writeln('Vuelva a intentarlo'),nl, readln(Input),validate_amount(Input,X).
-
 validate_amount([H|_],X):- (numberatom(H) -> X = H ,!).
 validate_amount([_|T],X):- validate_amount(T,X).
 
 
+
+/*
+    Receives a question as a sentence and prints the answer on the terminal.
+    @param        List:  question sent by the user as a list.
+
+*/
 validate_question([]):- nl,write('No entendi su pregunta'),nl,writeln('Vuelva a intentarlo'),nl.
 validate_question([H|N]):-read_question(H,P),is_comida(N,Val,Z),(P == 1,Z==1 -> nl,write('Menu de comida: '),write(Val),nl,comida([Val,Menu,_]),print_subcategorie(Menu)),!.
 validate_question([H|N]):-read_question(H,P),is_comida(N,Z),(P==1,Z==0 -> nl,write('-Tenemos disponible: '),nl,get_comidas(Cat),print_categorie(Cat)),!.
@@ -148,6 +171,12 @@ validate_question([H|_]):-is_bebida(H,Val,Z),( Z==1 -> nl,write('Menu de bebida:
 validate_question([H|N]):-read_question(H,P),is_bebida(N,Z),(P == 2,Z==0 -> nl,write('Tenemos disponible: '),nl,get_bebidas(Cat),print_categorie(Cat)),!.
 validate_question([_|N]):-validate_question(N).
 
+/*
+    Receives a atom and cheks if its a value for a question, returns an int to indicate wich question is asking.
+    @param        H:  atom with the question value.
+    @param        Z:  int that indicates the question type.
+
+*/
 read_question(H,Z):-  z = 0.
 read_question(H,Z):- (H=='comida' -> Z=1,! ;H=='comidas' -> Z=1,!).
 read_question(H,Z):- (H=='bebida' -> Z=2,! ;H=='bebidas' -> Z=2,!).
@@ -157,7 +186,13 @@ numberatom(Atom) :-
    atom_chars(Atom, Chs),
     catch(number_chars(_, Chs), error(syntax_error(_),_), false).
 
-% Revisa si un valor pertenece a la categoria principal o subcategoria
+
+/*
+    Checks if user input is part of a sublist in the database, returns the principal categorie of the input.
+    @param        X:  type of food, drink or place set by the user
+    @param        Y:  principal categorie of the input
+
+*/
 check_sub(X,Y):- get_comidas(Ca),member_val(X,Ca,Za,Val),(Za==1,Y = Val).
 check_sub(X,Y):- get_bebidas(Ca),member_val(X,Ca,Za,Val),(Za==1,Y = Val).
 check_sub(X,Y):- get_lugares(Ca),member_val(X,Ca,Za,Val),(Za==1,Y = Val).
@@ -167,8 +202,12 @@ check_sub(X,Y):- get_lugares2(Ca),member_listVal(X,Ca,Za,Val),(Za==1,lugar([Type
 
 
 
-% Revisa si la cantidad de personas que van es menor que la capacidad
-% del restaurante.
+/*
+    receives 2 numbers and checks if the amount of people is less than the restaurant capacity.
+    @param        CapInp:  amount of people going to the restaurant.
+    @param        CapRest:  capacity of the restaurant.
+   
+*/
 capacidad_Val(CapInp,CapRest):- atom_number(CapRest,R),(CapInp<R -> true; false).
 
 
@@ -211,7 +250,9 @@ printAyuda():-nl,write('-Este es el menu de ayuda!'),nl,write('Puede hacerle pre
 
 
 
-%                      Inicia el sistema experto
+/*
+    Starts the Expert System to ask the user about wich food, drinks, place and amount of people for the restaurant search
+*/
 startES:-
     writeln('
  _________________________________________
@@ -221,14 +262,14 @@ startES:-
 
     writeln('-Hola, Bienvenido a RestauranTEC!'),
     nl,writeln('-Para pedir informacion puede escribir "ayuda" '),nl,
-    writeln("-que tipo de comida se te antoja?"),nl,
+    writeln('-que tipo de comida se te antoja?'),nl,
     readln(Oracion1),
     valid_sentence(Oracion1,Oracionver),
     validate_input(Oracionver,TipoComida),
     %nl,write('comida ingresada: '),write(TipoComida),nl,
 
 
-    writeln("-que tipo de bebida desea tomar?"),
+    writeln('-que tipo de bebida desea tomar?'),
     nl,
     readln(Oracion2),
     valid_sentence(Oracion2,Oracionver2),
@@ -236,9 +277,9 @@ startES:-
     %nl,write('bebida ingresada: '),write(TipoBebida),nl,
 
 
-    writeln("-En que zona esta buscando el restaurante?"),
+    writeln('-En que zona esta buscando el restaurante?'),
     nl,
-    writeln("-Las zonas que cubrimos son: "),nl,
+    writeln('-Las zonas que cubrimos son: '),nl,
     get_lugares(Lug),
     print_categorie(Lug),
     readln(Oracion3),
